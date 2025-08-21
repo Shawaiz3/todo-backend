594f0585f106de7df7ccab15d3bc3d4308941315
Shawaiz Ahmed <shawaizahmed010@gmail.com>
2025-08-21 17:08:20 +0500

    added ownership of todos

--- src/models/todo.model.ts
+++ src/models/todo.model.ts
@@ -1,12 +1,19 @@
 import mongoose from "mongoose";
 type todoType = Document & {
     task: string
+    userId: mongoose.Types.ObjectId; 
 }
 const todoSchema = new mongoose.Schema<todoType>({
     task: {
         type: String,
         required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // foreign key
+        ref: "User",
+        required: true
     }
+
 }, { timestamps: true })
 const todoModel = mongoose.model<todoType>("tasks", todoSchema);
 export default todoModel;

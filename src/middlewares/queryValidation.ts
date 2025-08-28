6d35c1e8664c238cd6b1b402c07581036016b9e0
Shawaiz Ahmed <shawaizahmed010@gmail.com>
2025-08-28 17:38:04 +0500

    Added Searching on todos

--- src/middlewares/queryValidation.ts
+++ src/middlewares/queryValidation.ts
@@ -3,5 +3,6 @@ import joi from 'joi'
 export const querySchema = joi.object({
     page: joi.number().integer().min(1).default(1),
     limit: joi.number().integer().min(1).default(5),
-    task: joi.string().strict().optional()
+    search: joi.string().strict().optional(),
+    status: joi.string().valid('pending', 'completed').optional()
 });
